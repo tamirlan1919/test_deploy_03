61cac97437c091599d5d2c26c553663cca732e9d
ibras0696 <ibras0696>
2025-03-16 00:38:18 +0300

    Создания Функции Для извлечения запретных символов в ветке ibra

--- ibra.py
+++ ibra.py
@@ -1,1 +1,16 @@
 #Напиши здесь код
+
+# Функция для извлечения запретных символов
+def func(name):
+    zapret_symbol = "\"\\\'!#$%&'()*+,-./:;<=>?@[\]^_`{|}~'"
+    new_st = ''
+    for i in name:
+        if i not in zapret_symbol:
+            new_st += i
+        else:
+            pass
+
+    return new_st
+
+
+print(func('Ибр@$@аг52@%и"4к2'))
